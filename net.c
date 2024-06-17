4f72c1660f50e4e9e994e8b68c7e707fa8504146
YAMAMOTO Masaya <pandax381@gmail.com>
2024-06-18 08:02:39 +0900

    Skeleton of step15

--- net.c
+++ net.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 #include "arp.h"
 #include "platform.h"
@@ -13,6 +14,13 @@ struct net_protocol {
     struct net_protocol *next;
     uint16_t type;
     net_protocol_handler_t handler;
+};
+
+struct net_protocol_queue_entry {
+    struct queue_entry entry;
+    struct net_device *dev;
+    size_t len;
+    /* data bytes exists after this structure. */
 };
 
 /*
@@ -165,6 +173,16 @@ net_protocol_register(uint16_t type, net_protocol_handler_t handler)
   return 0;
 }
 
+static struct net_protocol_queue_entry *
+net_protocol_queue_push(struct net_protocol *proto, const uint8_t *data, size_t len, struct net_device *dev)
+{
+}
+
+static struct net_protocol_queue_entry *
+net_protocol_queue_pop(struct net_protocol *proto)
+{
+}
+
 int
 net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
@@ -182,6 +200,12 @@ net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev
   return 0;
 }
 
+void
+net_softirq_handler(unsigned int irq, void *arg)
+{
+}
+
+#include "arp.h"
 #include "ip.h"
 #include "icmp.h"
 
