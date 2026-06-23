85b5faca574f143265dfade319febab50ccbc1a2
sasakiy84 <sincerely.sasakiy84@gmail.com>
2026-06-23 16:32:54 +0000

    Completion of step2

--- net.c
+++ net.c
@@ -45,6 +45,12 @@ static int net_device_open(struct net_device *dev) {
     errorf("already opened, dev=%s", dev->name);
     return -1;
   }
+  if (dev->ops->open) {
+    if (dev->ops->open(dev) == -1) {
+      errorf("failure, dev=%s" , dev->name);
+      return -1;
+    }
+  }
   dev->flags |= NET_DEVICE_FLAG_UP;
   return 0;
 }
@@ -54,6 +60,12 @@ static int net_device_close(struct net_device *dev) {
   if (!NET_DEVICE_IS_UP(dev)) {
     errorf("not opened, dev=%s", dev->name);
     return -1;
+  }
+  if (dev->ops->close) {
+    if (dev->ops->close(dev) == -1) {
+      errorf("failure, dev=%s", dev->name);
+      return -1;
+    }
   }
   dev->flags &= ~NET_DEVICE_FLAG_UP;
   return 0;
@@ -70,12 +82,23 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
     errorf("too long, dev=%s, mtu=%u, len=%zu", dev->name, dev->mtu, len);
     return -1;
   }
+  if (!dev->ops->output) {
+    errorf("output callback function is not set, dev=%s", dev->name);
+    return -1;
+  }
+  if (dev->ops->output(dev, type, data, len, dst) == -1) {
+    errorf("failure, dev=%s, len=%zu", dev->name, len);
+    return -1;
+  }
   return 0;
 }
 
 int
 net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
+  debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+  debugdump(data, len);
+  return 0;
 }
 
 int
