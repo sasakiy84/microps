baa5ee60e9fe70936541e8d661acb32b78bcc9d8
sasakiy84 <sincerely.sasakiy84@gmail.com>
2026-07-22 09:12:01 +0000

    Completion of step15

--- platform/linux/platform.c
+++ platform/linux/platform.c
@@ -7,7 +7,8 @@
 #include "platform.h"
 
 #include "intr.h"
-#include "util.h"
+#include "net.h"
+
 
 int
 platform_init(void)
@@ -16,6 +17,7 @@ platform_init(void)
     if (intr_init() == -1) {
         return -1;
     }
+    intr_register(INTR_IRQ_SOFT, net_softirq_handler, 0, NULL);
     if (timer_init() == -1) {
         return -1;
     } 
