2a54737380b83884a9aee5527bb0743f60187122
sasakiy84 <sincerely.sasakiy84@gmail.com>
2026-07-22 10:55:22 +0000

    Completion of step17

--- net.c
+++ net.c
@@ -8,6 +8,7 @@
 #include "platform.h"
 
 #include "net.h"
+#include "udp.h"
 #include "util.h"
 
 struct net_protocol {
@@ -279,6 +280,10 @@ net_init(void)
     }
     if (icmp_init() == -1) {
       errorf("icmp_init() failure");
+      return -1;
+    }
+    if (udp_init() == -1) {
+      errorf("udp_init() failure");
       return -1;
     }
     infof("success");
