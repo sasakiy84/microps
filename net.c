72857007803b32b96a2c00b7999cdf884a7f4590
sasakiy84 <sincerely.sasakiy84@gmail.com>
2026-07-27 14:22:25 +0000

    Completion of step20

--- net.c
+++ net.c
@@ -8,6 +8,7 @@
 #include "platform.h"
 
 #include "net.h"
+#include "tcp.h"
 #include "udp.h"
 #include "util.h"
 
@@ -284,6 +285,10 @@ net_init(void)
     }
     if (udp_init() == -1) {
       errorf("udp_init() failure");
+      return -1;
+    }
+    if (tcp_init() == -1) {
+      errorf("tcp_init() failure");
       return -1;
     }
     infof("success");
